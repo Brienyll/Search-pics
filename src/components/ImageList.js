ebaa0f33f0703515bc615b67e2c82956ef4c2f7d
Brienyll <Brienyll29@gmail.com>
2019-08-23 01:37:53 -0700

    added images map

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const ImageList = (props) => {
+  const images = props.images.map(image => {
+    return <img src={image.urls.small} />
+  })
+
   return (
     <div>
-      ImageList
+      {images}
     </div>
   )
 }
